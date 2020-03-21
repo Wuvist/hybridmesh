aaa882a899ead61220da79c5883afe6c9edd5ad8
Wuvist <wuvist@gmail.com>
2020-03-21 17:37:17 +0800

    Code clean up

--- server/src/test/kotlin/com/blogwind/hybridmesh/UserControllerSpec.kt
+++ server/src/test/kotlin/com/blogwind/hybridmesh/UserControllerSpec.kt
@@ -4,23 +4,18 @@ import io.kotlintest.matchers.string.shouldContain
 import io.kotlintest.specs.StringSpec
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
-import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.annotation.MicronautTest
 import javax.inject.Inject
 
 @MicronautTest
 class UserControllerSpec : StringSpec() {
     @Inject
-    lateinit var server: EmbeddedServer
-
-    @Inject
     @field:Client("/")
     lateinit var client: HttpClient
 
     init {
         "dummy test" {
-            val rsp: String = client.toBlocking()
-                    .retrieve("/user/login")
+            val rsp: String = client.toBlocking().retrieve("/user/login")
             rsp shouldContain "action='/user/login'"
         }
     }
